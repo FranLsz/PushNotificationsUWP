9de9db693a8011b94ec09a83993ae6306dbe256d
Fran <franlsz95develop@gmail.com>
2016-02-23 01:08:42 +0100

    Borrado modelo sobrante

--- PushUniversalWindows/Model/TodoItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PushUniversalWindows.Model
-{
-    public class TodoItem
-    {
-        public string Id { get; set; }
-        public string Text { get; set; }
-        public bool Complete { get; set; }
-    }
-}
